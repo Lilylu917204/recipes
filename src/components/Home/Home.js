fc24d52e7eafb9abbbfe1c475f2b3e1d40bff1fc
lilylu <peggy9172045@gmail.com>
2021-11-01 21:36:39 +0800

    fetch mealtype

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,9 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchBreakfast } from "../../features/appSlice";
 import RecipeListing from "../RecipeListing/RecipeListing";
-import { getBreakfast } from "../../features/appSlice";
-import { useSelector } from "react-redux";
 
 function Home() {
   const dispatch = useDispatch();
-  const breakfast = useSelector(getBreakfast);
 
   useEffect(() => {
     dispatch(fetchBreakfast());
