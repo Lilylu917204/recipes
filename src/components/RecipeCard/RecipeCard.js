b099941185d85d6f4d35f3f35ee2f308f7e09e66
lilylu <peggy9172045@gmail.com>
2021-11-02 15:49:07 +0800

    router details

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 
 function RecipeCard({ data }) {
   const recipeUri = data.recipe.uri.split(/([_])/)[2];
+  const mealType = data.recipe.mealType;
   return (
     <div>
-      <Link to={`/recipe/${recipeUri}`}></Link>
-      {/* test */}
-      {data.recipe.label}
+      <Link to={`/recipe/${mealType}/${recipeUri}`}>
+        {/* test */}
+        {data.recipe.label}
+      </Link>
     </div>
   );
 }
