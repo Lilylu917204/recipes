bda9e4317178aa42d303ddd7991b5e87e6bdb66d
lilylu <peggy9172045@gmail.com>
2021-11-07 17:10:42 +0800

    revise recipe id

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function RecipeCard({ data }) {
-  const recipeUri = data.recipe.uri.split(/([_])/)[2];
+  const dataUrl = data.recipe.uri;
+  const recipeId = dataUrl.split("recipe_")[1];
+
+  // 可惡的樹懶 正規表達式
+  // const recipeUri = data.recipe.uri.split(/([recipe_])/)[2];
 
   const mealType = data.recipe.mealType;
 
   return (
     <div>
-      <Link to={`/recipe/${mealType}/${recipeUri}`}>
+      <Link to={`/recipe/${mealType}/${recipeId}`}>
         {/* test */}
         {data.recipe.label}
       </Link>
